7898eeaa0aedd62d135f4cbc75662afdee5937a9
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-07-05 18:40:59 -0300

    Special move Promotion

--- xadrez-console/xadrex/PartidaXadrez.cs
+++ xadrez-console/xadrex/PartidaXadrez.cs
@@ -144,6 +144,21 @@ namespace xadrex
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -162,8 +177,6 @@ namespace xadrex
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
